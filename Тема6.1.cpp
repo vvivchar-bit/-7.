fa9ec3e57592abb137121a79ddbd36fca7c50ab0
vvivchar-bit <v.vivchar@istu.edu.ua>
2026-04-03 11:48:07 +0300

    Add files via upload

---
+++ Тема6.1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+class IExecutable {
+public:
+    virtual int start() = 0;
+    virtual void run() = 0;
+    virtual void end() = 0;
+
+    virtual ~IExecutable() {}
+};
+
+class DownloadTask : public IExecutable {
+public:
+    int start() override {
+        cout << "DownloadTask started" << endl;
+        return 1;
+    }
+
+    void run() override {
+        cout << "Downloading data..." << endl;
+    }
+
+    void end() override {
+        cout << "DownloadTask finished" << endl;
+    }
+};
+
+class BackupTask : public IExecutable {
+public:
+    int start() override {
+        cout << "BackupTask started" << endl;
+        return 1;
+    }
+
+    void run() override {
+        cout << "Creating backup..." << endl;
+    }
+
+    void end() override {
+        cout << "BackupTask finished" << endl;
+    }
+};
+
+void executeCallback(IExecutable* task) {
+    if (task->start()) {
+        task->run();
+        task->end();
+    }
+}
+
+int main() {
+    DownloadTask d;
+    BackupTask b;
+
+    executeCallback(&d);
+    cout << endl;
+    executeCallback(&b);
+
+    return 0;
+}
---
+++ Тема7.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+
+class IExecutable {
+public:
+    virtual int start() = 0;
+    virtual void run() = 0;
+    virtual void end() = 0;
+
+    virtual ~IExecutable() {
+        cout << "IExecutable destructor" << endl;
+    }
+};
+
+class Work : public IExecutable {
+public:
+    int start() override {
+        cout << "Work started" << endl;
+        return 1;
+    }
+
+    void run() override {
+        cout << "Work is running" << endl;
+    }
+
+    void end() override {
+        cout << "Work ended" << endl;
+    }
+
+    ~Work() {
+        cout << "Work destructor" << endl;
+    }
+};
+
+class Game : public IExecutable {
+public:
+    int start() override {
+        cout << "Game started" << endl;
+        return 1;
+    }
+
+    void run() override {
+        cout << "Game is running" << endl;
+    }
+
+    void end() override {
+        cout << "Game ended" << endl;
+    }
+
+    ~Game() {
+        cout << "Game destructor" << endl;
+    }
+};
+
+int main() {
+    IExecutable* obj1 = new Work();
+    IExecutable* obj2 = new Game();
+
+    obj1->start();
+    obj1->run();
+    obj1->end();
+
+    cout << endl;
+
+    obj2->start();
+    obj2->run();
+    obj2->end();
+
+    delete obj1;
+    delete obj2;
+
+    return 0;
+}
